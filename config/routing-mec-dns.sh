7107407e735a6bae4d6cbe61f7276efefbb6859f
Louis Royer <55180044+louisroyer@users.noreply.github.com>
2021-11-25 11:18:44 +0100

    Anycast for DNS

--- config/routing-mec-dns.sh
+++ config/routing-mec-dns.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-ip route add 10.0.222.0/24 via 10.0.223.2
-ip route add 10.0.224.0/24 via 10.0.223.2
+MNO_DNS_PUBLIC=10.0.224.3
+MEC_UPF=10.0.223.2
+ip route add 10.0.222.0/24 via $MEC_UPF
+ip route add 10.0.218.0/24 via 10.0.217.3
+ip addr add $MNO_DNS_PUBLIC dev $(ip route get $MEC_UPF|awk '{print $3;exit}')
