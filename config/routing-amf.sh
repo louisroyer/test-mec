bf059235e7afc671d3bb1767810078d88f8b3133
Louis Royer <55180044+louisroyer@users.noreply.github.com>
2021-11-29 13:00:42 +0100

    AMF ipv6

--- config/routing-amf.sh
+++ config/routing-amf.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # route gnb1
 ip route add 10.0.213.0/24 via 10.0.214.4
+ip route add fd00:d0cc:e700:1111:2::/80 via fd00:d0cc:e700:1111:3::4
--- config/routing-gnb.sh
+++ config/routing-gnb.sh
@@ -2,3 +2,4 @@
 
 # route amf
 ip route add 10.0.214.0/24 via 10.0.213.4
+ip route add fd00:d0cc:e700:1111:3::/80 via fd00:d0cc:e700:1111:2::4
